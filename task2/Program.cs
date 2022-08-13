3989578db702fcbeda74b11909baeb1fd91fc784
Pavel <p_v_s@mail.ru>
2022-08-13 19:00:41 +0700

    Первый коммит. Добавлены 2 задачи

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача. Работа с массивами. Упорядочить массив.
+Методом выбора (минимакса ещё его называют). */
+
+
+
+void PrintArray(int[] array)
+{
+int count = array.Length;
+    for(int i=0;i<count;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int [] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        int minPosition = i;
+        for(int j=i+1;j<array.Length;j++)
+        {
+            if(array[j]<array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i]=array[minPosition];
+        array[minPosition]=temporary;
+    }
+}
+
+int []arr = {1,5,4,3,2,6,7,1,1};
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
